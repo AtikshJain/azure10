8169266a3f1bc0679a6be7bc62da2803dfb948e0
AtikshJain <atja22ise@cmrit.ac.in>
2025-04-15 15:35:19 +0530

    my first azure ci

---
+++ pgm10/src/main/java/com/atiksh/App.java
@@ -0,0 +1,13 @@
+package com.atiksh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
